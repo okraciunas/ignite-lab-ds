8b20e33a9569eb31b6125617edba21b3c6ad0c5a
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2022-10-13 18:19:44 -0300

    props componente button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,21 +1,14 @@
+import { ButtonHTMLAttributes } from 'react'
 import classnames from 'classnames'
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: string
   fullWidth?: boolean
 }
 
-export function Button({ children, fullWidth = false }: ButtonProps) {
+export function Button({ children, className, fullWidth = false, ...props }: ButtonProps) {
   return (
-    <button className={classnames(`
-      py-4 px-3
-      font-semibold text-sm text-black
-      rounded bg-cyan-500
-      transition-colors
-      hover:bg-cyan-300
-      focus:ring-2 ring-white`,
-      { 'w-full': fullWidth }
-    )}>
+    <button className={classnames('py-3 px-4 font-semibold text-sm text-black rounded bg-cyan-500 transition-colors hover:bg-cyan-300 focus:ring-2 ring-white', { 'w-full': fullWidth }, className)} {...props}>
       {children}
     </button>  
   )
