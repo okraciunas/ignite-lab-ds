50d2dd2d4087ba1d8c1ebcd0ce4dd5f0918cd1d1
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2022-10-13 18:18:11 -0300

    component reactlogo + stories

---
+++ src/components/react-logo/stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { ReactLogo } from '.'
+
+export default {
+  title: 'Components/ReactLogo',
+  component: ReactLogo
+} as Meta
+
+export const Default: StoryObj = {}
