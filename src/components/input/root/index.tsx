1bd2d915c960a500c599b652beda0a93a01a5244
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2022-10-12 21:53:54 -0300

    ajuste component input

--- src/components/input/root/index.tsx
+++ src/components/input/root/index.tsx
@@ -6,7 +6,7 @@ export interface InputRootProps {
 
 export function InputRoot({ children }: InputRootProps) {
   return (
-    <div className='flex item-center gap-3 py-4 px-3 outline-none rounded bg-gray-800 focus-within:ring-2 ring-cyan-300'>
+    <div className='flex items-center gap-3 h-12 py-4 px-3 outline-none rounded bg-gray-800 focus-within:ring-2 ring-cyan-300'>
       {children}
     </div>
   )
