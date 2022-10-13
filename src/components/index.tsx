2c2b69130a84067ff35f1d6a1cf17a48e01b0a0d
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2022-10-13 18:18:28 -0300

    export de components

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+import { Button } from './button'
+import { Checkbox } from './checkbox'
+import { Heading } from './heading'
+import { Input } from './input'
+import { ReactLogo } from './react-logo'
+import { Text } from './text'
+
+export {
+  Button,
+  Checkbox,
+  Heading,
+  Input,
+  ReactLogo,
+  Text
+}
