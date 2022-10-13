af15e6b36755c52d8163cb6e9400360f5759cd47
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2022-10-13 10:51:06 -0300

    config ci storybook deploy

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,25 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*stories.mdx",
     "../src/**/*stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite"
   },
-  "features": {
+  features: {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/ignite-lab-ds/'
+    }
+
+    return config
   }
 }
