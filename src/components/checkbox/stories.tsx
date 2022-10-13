6d0a1f145adf89e57970ec805f5f5397e4253911
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2022-10-12 22:06:10 -0300

    component checkbox + storie

---
+++ src/components/checkbox/stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Checkbox, CheckboxProps } from '.'
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox
+} as Meta<CheckboxProps>
+
+export const Default: StoryObj<CheckboxProps> = {}
