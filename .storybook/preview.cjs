dadcbeab25c656654bc47880f60649a60ebba46f
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2022-10-13 22:11:40 -0300

    config msw

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,6 +1,11 @@
 import { themes } from '@storybook/theming'
+import { initialize as mswinitialize, mswDecorator } from 'msw-storybook-addon';
 
 import './../src/styles/global.css'
+
+mswinitialize({ onUnhandledRequest: 'bypass' });
+
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
